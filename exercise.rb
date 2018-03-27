4e15b4e80d64718eb2f0ac5f9a9d876969ffd47a
Michael Cleary <mcleary03@gmail.com>
2018-03-27 11:40:32 -0700

    implement marklar, even_fibbonacci, and getFib methods on Exercise class; passes all specs

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,15 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    words = str.split(' ')
+    words.map { |word|
+      punctuation = word.index(/\W/) ? word[word.index(/\W/)] : ''
+      word.length > 4 ?
+        word = word==word.capitalize ?
+          'Marklar'+punctuation :
+            'marklar'+punctuation :
+              word+punctuation
+    }.join(' ')
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +20,19 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    fibs = []
+
+    (0..nth).each { |n|
+      fibs.push( self.getFib(n, fibs) )
+    }
+
+    fibs.select{ |n| n % 2 == 0 }.reduce(&:+)
+  end
+
+  def self.getFib(n, fibs)
+    return n if n <= 1
+
+    fibs[n-1] + fibs[n-2]
   end
 
 end
